5d74672076c8e72c885481d15f731595434bcf06
vaishanvi-2004 <vaishnavilohote2004@gmail.com>
2025-12-04 16:50:02 +0000

    cm2

---
+++ ndex.js
@@ -0,0 +1,9 @@
+                var express = require('express');
+                 
+                var app = express();//Respond with "hello world" for requests that hit our root "/"
+                app.get('/', function (req, reis) {
+                 res.send('Welcome to jenkins!! ');
+                });//listen to port 3000 by default
+                app.listen(process.env.PORT || 3000);
+                 
+                module.exports = app;
